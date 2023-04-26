7ae379ad9f68eb401324138d4891d981816831de
praduman2714 <pradu14jsr@gmail.com>
2023-04-26 16:24:20 +0530

    first commit

---
+++ src/Moviecard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Moviecard extends React.Component{
+    render() {
+      return (
+        <div>
+          <h2>Movie Card Brother</h2>
+        </div>
+      )
+    }
+}
+
+
+export default Moviecard;
